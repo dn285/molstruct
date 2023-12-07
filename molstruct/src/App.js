d4764d84951d310e83879e05fa43b37a39859477
dn285 <dn285@cornell.edu>
2023-12-06 21:04:02 -0600

    Add support for drawing C, N, O, H atoms

--- molstruct/src/App.js
+++ molstruct/src/App.js
@@ -2,10 +2,10 @@ import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
-    const [selectedTool, setSelectedTool] = useState('dot');
-    const [dots, setDots] = useState([]);
-    const [lines, setLines] = useState([]);
-    const [selectedDots, setSelectedDots] = useState([]);
+    const [selectedTool, setSelectedTool] = useState('C');
+    const [atoms, setAtoms] = useState([]);
+    const [bonds, setBonds] = useState([]);
+    const [selectedAtoms, setSelectedAtoms] = useState([]);
     const canvasRef = useRef(null);
 
     useEffect(() => {
@@ -15,39 +15,41 @@ function App() {
         // Clear canvas
         context.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Draw dots
-        dots.forEach(dot => {
-            context.beginPath();
-            context.arc(dot.x, dot.y, 5, 0, 2 * Math.PI);
-            context.fill();
-        });
+        // Draw atoms
+        atoms.forEach(atom => {
+            context.font = "20px Arial";
+            context.fillText(atom.type, atom.x, atom.y);
+        })
 
-        // Draw lines
-        lines.forEach(line => {
+        // Draw bonds
+        bonds.forEach(bond => {
             context.beginPath();
-            context.moveTo(line.start.x, line.start.y);
-            context.lineTo(line.end.x, line.end.y);
+            context.moveTo(bond.start.x, bond.start.y);
+            context.lineTo(bond.end.x, bond.end.y);
             context.stroke();
         });
-    }, [dots, lines]);
+    }, [atoms, bonds]);
 
     const handleCanvasClick = (event) => {
         const rect = canvasRef.current.getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
-        const clickedDot = dots.find(dot => Math.hypot(dot.x - x, dot.y - y) < 5);
 
-        if (selectedTool === 'dot') {
-            setDots([...dots, { x, y }]);
+        const clickedAtom = atoms.find(atom =>
+            Math.abs(atom.x - x) < 10 && Math.abs(atom.y - y) < 10);
+
+        if (['C', 'N', 'O', 'H'].includes(selectedTool)) {
+            setAtoms([...atoms, { type: selectedTool, x, y }]);
+            setSelectedAtoms([]);
         }
-        else if (selectedTool == 'line') {
-            if (clickedDot) {
-                if (selectedDots.length < 1) {
-                    setSelectedDots([clickedDot]);
+        else if (selectedTool == 'bond') {
+            if (clickedAtom) {
+                if (selectedAtoms.length < 1) {
+                    setSelectedAtoms([clickedAtom]);
                 }
                 else {
-                    setLines([...lines, { start: selectedDots[0], end: clickedDot }])
-                    setSelectedDots([]);
+                    setBonds([...bonds, { start: selectedAtoms[0], end: clickedAtom }])
+                    setSelectedAtoms([]);
                 }
             }
         }
@@ -56,8 +58,11 @@ function App() {
     return (
         <div className="App">
             <div className="toolbar">
-                <button onClick={() => setSelectedTool('dot')}>Dot Tool</button>
-                <button onClick={() => setSelectedTool('line')}>Line Tool</button>
+                <button onClick={() => setSelectedTool('C')}>C</button>
+                <button onClick={() => setSelectedTool('N')}>N</button>
+                <button onClick={() => setSelectedTool('O')}>O</button>
+                <button onClick={() => setSelectedTool('H')}>H</button>
+                <button onClick={() => setSelectedTool('bond')}>Bond</button>
             </div>
             <canvas ref={canvasRef} width="800" height="600" onClick={handleCanvasClick} className="drawing-area" />
         </div>
